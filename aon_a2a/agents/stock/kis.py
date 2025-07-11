2d8c2f0ea016f42889f0e67a667908cda6d66d0a
myeonggil <echosoul1994@gmail.com>
2025-07-11 19:14:16 +0900

    [UPDATE] stock event

--- aon_a2a/agents/stock/kis.py
+++ aon_a2a/agents/stock/kis.py
@@ -2,19 +2,19 @@ from typing import Optional
 
 from aon_a2a.configs import config
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class RequestHeader:
-    content_type: str    #컨텐츠타입
-    authorization: str    #접근토큰
-    appkey: str    #앱키
-    appsecret: str    #앱시크릿키
+    content_type: str = None    #컨텐츠타입
+    authorization: str = None    #접근토큰
+    appkey: str = None    #앱키
+    appsecret: str = None    #앱시크릿키
     personalseckey: Optional[str] = None    #고객식별키
-    tr_id: str    #거래ID
+    tr_id: str = None    #거래ID
     tr_cont: Optional[str] = None    #연속 거래 여부
-    custtype: str    #고객 타입
+    custtype: str = None   #고객 타입
     seq_no: Optional[str] = None    #일련번호
     mac_address: Optional[str] = None    #맥주소
     phone_number: Optional[str] = None    #핸드폰번호
@@ -24,5 +24,31 @@ class RequestHeader:
 
 @dataclass
 class RequestQueryParam:
-    FID_COND_MRKT_DIV_CODE: str    #조건 시장 분류 코드
-    FID_INPUT_ISCD: str    #입력 종목코드
+    FID_COND_MRKT_DIV_CODE: str = None    #조건 시장 분류 코드
+    FID_INPUT_ISCD: str = None    #입력 종목코드
+
+
+@dataclass
+class StockInfo:
+    stock_event: str = None
+    stock_code: int = 0
+
+@dataclass
+class Stock:
+    stock_name: str = None
+    stock_info: StockInfo = None
+
+
+
+from numbers_parser import Document
+doc = Document("./code.numbers")
+sheets = doc.sheets
+tables = sheets[0].tables
+rows = tables[0].rows()
+stock_name = rows[1][1].value
+stock_code = int(rows[1][0].value)
+stock_event = rows[1][2].value
+
+stock_info = StockInfo(stock_event=stock_event, stock_code=stock_code)
+stock = Stock(stock_name=stock_name, stock_info=stock_info)
+print(stock)
