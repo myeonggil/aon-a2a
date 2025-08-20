f41375ac868d4240ca98a9264033ce276b3ab2f5
myeonggil <echosoul1994@gmail.com>
2025-08-20 16:21:32 +0900

    [UPDATE] litestar draft

--- aon_a2a/main.py
+++ aon_a2a/main.py
@@ -1,13 +1,43 @@
 import msgspec
 
-from typing import Annotated
+from typing import Annotated, Optional, Any
+from dataclasses import dataclass
 
-from litestar import Litestar, get, post, Controller
-from litestar.dto.msgspec_dto import MsgspecDTO
+from litestar import Litestar, get, post, Controller, Request
+from litestar.security.jwt import JWTAuth, Token
+from litestar.dto import MsgspecDTO
+from litestar.params import Parameter
+from litestar.di import Provide
+
+# Litestar - manual auth setup
+from litestar.middleware import AbstractAuthenticationMiddleware
+import jwt
 
 
-class Request(msgspec.Struct):
-    name: Annotated[str, None]
+# class JWTAuthenticationMiddleware(AbstractAuthenticationMiddleware):
+#     async def authenticate_request(self, connection):
+#         token = connection.headers.get("Authorization", "").replace("Bearer ", "")
+#         if token:
+#             try:
+#                 payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+#                 return await get_user_by_id(payload["user_id"])
+#             except jwt.JWTError:
+#                 return None
+#         return None
+
+
+
+async def varify_authentication(headers: dict):
+    return True
+
+
+@dataclass
+class CustomToken(Token):
+    token_flag: bool = False
+
+
+class Inside(msgspec.Struct):
+    name: Optional[str] = None
 
 
 class Response(msgspec.Struct):
@@ -18,14 +48,16 @@ class RequestResponse(Controller):
 
     path = "/test"
 
-    @get("/inside")
-    async def inside(self, name: str) -> Response:
-        print(name)
+    @get(
+        path="/inside",
+        dependencies={"local_dependency": Provide(varify_authentication)}
+    )
+    async def inside(self, inside: Inside, local_dependency: bool = False) -> Response:
+        print(local_dependency)
         return Response(content="Hello World!")
 
-    @post("/outside")
+    @post(path="/outside")
     async def outside(self, request: Request) -> Response:
-        print(request.name)
         return Response(content="Hello World!")
 
 
