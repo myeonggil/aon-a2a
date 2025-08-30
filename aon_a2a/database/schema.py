d10053031e4a881117fec8e299443d25a4650481
myeonggil <echosoul1994@gmail.com>
2025-08-30 17:30:25 +0900

    [UPDATE] draft pytest and version

--- aon_a2a/database/schema.py
+++ aon_a2a/database/schema.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm.decl_api import DeclarativeBase
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
 from datetime import datetime
